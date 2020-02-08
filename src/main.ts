d1a8d823e5b63c244a85a644a480809c9af4313e
marony <marony@binbo-kodakusan.com>
2020-02-09 00:28:39 +0900

    リファクタリング

--- src/main.ts
+++ src/main.ts
@@ -5,36 +5,24 @@ const message: string = 'Hello World';
 // sub.jsに定義されたJavaScriptを実行する。
 hello(message);
 
-const str2ab = (str: string): ArrayBuffer => {
-    const buf = new ArrayBuffer(str.length * 2);
-    const bufView = new Uint16Array(buf);
-    for (let i = 0; i < str.length; ++i) {
-        bufView[i] = str.charCodeAt(i);
-    }
-    return buf;
-}
-
 export function play() {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', './sounds/2608_bd.wav', true);
-    xhr.responseType = 'arraybuffer';
-    console.log(xhr.responseType);
-    console.log("1");
-    xhr.onload = function() {
-        let arrayBuffer = xhr.response;
+    let AudioContext = window.AudioContext;// || window.webkitAudioContext;
+    let ac = new AudioContext();
+    let source = ac.createBufferSource();
+    source.connect(ac.destination);
+    source.start(0);
 
-        let AudioContext = window.AudioContext;// || window.webkitAudioContext;
-        let ac = new AudioContext();
-        ac.decodeAudioData(arrayBuffer, function(audioBuffer) {
-            console.log("3");
-            let source = ac.createBufferSource();
-            source.buffer = audioBuffer;
-            source.connect(ac.destination);
-            source.start(0);
+    // 音声ファイルを読み込む
+    fetch('./sounds/2608_bd.wav')
+        .then(res => res.arrayBuffer())
+        .then(compressed => {
+            ac.decodeAudioData(compressed)
+                .then(decoded => {
+                    source.buffer = decoded;
+                });
         });
-    };
-    xhr.send();
 }
 
+// クリックされたらplay()を呼ぶ
 let button = document.getElementById('play');
 button?.addEventListener('click', (e: Event) => play());
--- src/sub.ts
+++ src/sub.ts
@@ -1,6 +1,5 @@
 // module.exportを使ってhello関数を定義する。
 export const hello = (message: string): void => {
-
   log(message);
 };
 
@@ -11,8 +10,3 @@ function log(message: string) :void{
 
   console.log(`${message}を出力しました`);
 }
-
-export function piyo(){
-
-}
-
