e329eda152e02f1a4d2c945e30c37dddb424dc12
marony <marony@binbo-kodakusan.com>
2020-02-12 14:22:21 +0900

    音声ファイルからオシレータに変更

---
+++ src/audio.ts
@@ -0,0 +1,55 @@
+export class Audio {
+    context: AudioContext;
+
+    constructor() {
+        window.AudioContext = window.AudioContext || (window as any).webkitAudioContext;
+        // Create the instance of AudioContext
+        this.context = new AudioContext();
+        // for legacy browsers
+        this.context.createGain = this.context.createGain || (this.context as any).createGainNode;
+    }
+
+    connectDestination(node: AudioNode) {
+        node.connect(this.context.destination);
+    }
+
+    createGain(): GainNode {
+        return this.context.createGain();
+    }
+
+    // OscillatorNode
+    // | type      | 波形を決定するためのプロパティ. 音色に大きく影響する. |
+    // | frequency | 周波数を決定するためのプロパティ. 音の高さ (ピッチ) に大きく影響する. |
+    // | detune    | 複数のサウンドを合成する場合において, 音の高さを微妙にずらして, サウンドに厚みを出したり, オクターブ違いの音をミックスしたりするのに利用する. 前者はシンセサイザーのファインチューン機能で, 後者はオクターバーエフェクト. |
+    // WaveType
+    // | 正弦波       | sine     | 0 |
+    // | 矩形波       | square   | 1 |
+    // | ノコギリ波   | sawtooth | 2 |
+    // | 三角波       | triangle | 3 |
+    // | カスタム波形 | custom   | 4 |
+    createOscillator(): OscillatorNode {
+        let oscillator = this.context.createOscillator();
+        // for legacy browsers
+        oscillator.start = oscillator.start || (oscillator as any).noteOn;
+        oscillator.stop  = oscillator.stop  || (oscillator as any).noteOff;
+        return oscillator;
+    }
+};
+
+/* 音声ファイル(WAV)を鳴らす
+    let AudioContext = window.AudioContext;// || window.webkitAudioContext;
+    let ac = new AudioContext();
+    let source = ac.createBufferSource();
+    source.connect(ac.destination);
+    source.start(0);
+
+    // 音声ファイルを読み込む
+    fetch('./sounds/2608_bd.wav')
+        .then(res => res.arrayBuffer())
+        .then(compressed => {
+            ac.decodeAudioData(compressed)
+                .then(decoded => {
+                    source.buffer = decoded;
+                });
+        });
+*/
--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,44 @@
-import {hello} from './sub';
-
-const message: string = 'Hello World';
-
-// sub.jsに定義されたJavaScriptを実行する。
-hello(message);
+import { Audio } from './audio';
 
 export function play() {
-    let AudioContext = window.AudioContext;// || window.webkitAudioContext;
-    let ac = new AudioContext();
-    let source = ac.createBufferSource();
-    source.connect(ac.destination);
-    source.start(0);
+    // OscillatorNode (Input) -> GainNode (Volume) -> AudioDestinationNode (Output)
+    const audio = new Audio();
+    let gain = audio.createGain()
+    let oscillator = audio.createOscillator();
+    audio.connectDestination(gain);
+    oscillator.connect(gain);
 
-    // 音声ファイルを読み込む
-    fetch('./sounds/2608_bd.wav')
-        .then(res => res.arrayBuffer())
-        .then(compressed => {
-            ac.decodeAudioData(compressed)
-                .then(decoded => {
-                    source.buffer = decoded;
-                });
-        });
+    oscillator.start(0);
+    window.setTimeout(() => oscillator.stop(), 1000);
+
+    // let AudioContext = window.AudioContext;// || window.webkitAudioContext;
+    // let ac = new AudioContext();
+    // let source = ac.createBufferSource();
+    // source.connect(ac.destination);
+    // source.start(0);
+
+    // // 音声ファイルを読み込む
+    // fetch('./sounds/2608_bd.wav')
+    //     .then(res => res.arrayBuffer())
+    //     .then(compressed => {
+    //         ac.decodeAudioData(compressed)
+    //             .then(decoded => {
+    //                 source.buffer = decoded;
+    //             });
+    //     });
 }
 
-// クリックされたらplay()を呼ぶ
-let button = document.getElementById('play');
-button?.addEventListener('click', (e: Event) => play());
+const main = () => {
+    // クリックされたらplay()を呼ぶ
+    let button = document.getElementById('play');
+    button?.addEventListener('click', (e: Event) => play());
+
+    // canvasに何か描くよ
+    const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+    
+    ctx.fillStyle = 'green';
+    ctx.fillRect(10, 10, 150, 100);
+};
+
+main();
