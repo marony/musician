065f2941c05f6feeb41ad6a3f22ac3e933b8f6d1
marony <marony@binbo-kodakusan.com>
2020-02-16 18:37:03 +0900

    いろいろ

--- src/audio_controller.ts
+++ src/audio_controller.ts
@@ -1,40 +1,80 @@
 import { Figure, EventHandler } from "./figure";
 import { Audio } from './audio';
 
-// UIと音声の橋渡し
-export class AudioController implements EventHandler<Figure> {
-    onMouseDown(fig: Figure): void {
-        this.play(fig);
+class Note {
+    note: number;
+    private oscillator: OscillatorNode | null = null;
+
+    constructor(note: number) {
+        this.note = note;
     }
 
-    onMouseUp(fig: Figure): void {
-    }
-
-    onMouseOver(fig: Figure): void {
-    }
-
-    onMouseOut(fig: Figure): void {
-    }
-
-    private play(fig: Figure): void {
-        const d = fig.note;
-        console.log('note = ' + d);
-        const hz = 440.0 * 2 ** (d / 12);
+    play(controller: AudioController) {
+        const hz = 440.0 * 2 ** (this.note / 12);
 
         const audio = new Audio();
         let gain = audio.createGain()
-        let oscillator1 = audio.createOscillator();
-        oscillator1.type = 'sawtooth';
-        let oscillator2 = audio.createOscillator();
-        oscillator2.type = 'triangle';
+        let oscillator = audio.createOscillator();
+        oscillator.type = 'sawtooth';
         audio.connectDestination(gain);
-        oscillator1.connect(gain);
-        oscillator1.frequency.value = hz;
-        oscillator2.connect(gain);
-        oscillator2.frequency.value = hz;
+        oscillator.connect(gain);
+        oscillator.frequency.value = hz;
 
-        oscillator1.start(0);
-        oscillator2.start(0);
-        window.setTimeout(() => { oscillator1.stop(); oscillator2.stop(); }, 1000);
+        oscillator.start(0);
+        this.oscillator = oscillator;
+    }
+
+    stop(controller: AudioController) {
+        const oscillator = this.oscillator;
+        if (!oscillator) {
+            return;
+        }
+        this.oscillator = null;
+
+        window.setTimeout(() => oscillator.stop(), 200);
+        // 下記だとダメ
+        // window.setTimeout(oscillator.stop, 200);
+    }
+}
+
+// UIと音声の橋渡し
+export class AudioController implements EventHandler<Figure> {
+    notes: Note[] = [];
+
+    onMouseDown(fig: Figure): void {
+        console.log(`onMouseDown: ${fig.note}`);
+        const index  = this.notes.findIndex((n) => fig.note == n.note)
+        if (index >= 0) {
+            // 既に鳴っているので何もしない
+            return;
+        }
+        // 音を鳴らす
+        const note = new Note(fig.note);
+        this.notes.push(note);
+        note.play(this);
+    }
+
+    onMouseUp(fig: Figure): void {
+        const index  = this.notes.findIndex((n) => fig.note == n.note)
+        console.log(`onMouseUp: ${fig.note}, ${index}`);
+        if (index < 0) {
+            // 鳴ってないので何もしない
+            return;
+        }
+        // 音を止める
+        const note = this.notes[index];
+        note.stop(this);
+        this.notes.splice(index, 1);
+    }
+
+    onMouseOver(fig: Figure): void {
+        console.log(`onMouseOver: ${fig.note}`);
+        // キャンバスに入った時にしか呼ばれないので
+        // マウスの移動の度に、各figに入った出たを自分で管理する
+    }
+
+    onMouseOut(fig: Figure): void {
+        console.log(`onMouseOut: ${fig.note}`);
+        // キャンバスから出たので全音を止める？
     }
 }
